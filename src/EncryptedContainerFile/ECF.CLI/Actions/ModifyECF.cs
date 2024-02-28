9cc8c1150a4056f9bf3555cb4323b018be9c3d07
Hirnmoder <19573355+Hirnmoder@users.noreply.github.com>
2024-02-28 16:57:19 +0100

    Enable external editors to modify ECF content (only on Linux)

--- src/EncryptedContainerFile/ECF.CLI/Actions/ModifyECF.cs
+++ src/EncryptedContainerFile/ECF.CLI/Actions/ModifyECF.cs
@@ -2,6 +2,7 @@
 using ECF.Core.Container;
 using ECF.Core.Container.Keys;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,21 +23,10 @@ namespace ECF.CLI.Actions
                 using var fs = File.Open(arg.Filepath, FileMode.Open, FileAccess.ReadWrite);
                 using var ec = EncryptedContainer.Load(fs, k);
 
-                using var sr = new StreamReader(ec.ContentStream, Encoding.UTF8, leaveOpen: true);
-                using var sw = new StreamWriter(ec.ContentStream, Encoding.UTF8, leaveOpen: true);
-                Task.Run(async () =>
-                {
-                    var te = new TextEditor(arg.Filepath, sr, () =>
-                    {
-                        sw.Flush();
-                        sw.BaseStream.Position = 0;
-                        sw.BaseStream.SetLength(0);
-                        return sw;
-                    }, 30);
-                    await te.RunAsync();
-                }).Wait();
-                sr.Close();
-                sw.Close();
+                if (string.IsNullOrWhiteSpace(arg.Editor))
+                    ExecuteWithYae(ec, arg);
+                else
+                    ExecuteWithExternal(ec, arg);
 
                 fs.SetLength(0); // Reset stream length
                 ec.Write(fs);
@@ -52,5 +42,64 @@ namespace ECF.CLI.Actions
             }
         }
 
+        private static void ExecuteWithYae(EncryptedContainer ec, ModifyArgument arg)
+        {
+            using var sr = new StreamReader(ec.ContentStream, Encoding.UTF8, leaveOpen: true);
+            using var sw = new StreamWriter(ec.ContentStream, Encoding.UTF8, leaveOpen: true);
+            Task.Run(async () =>
+            {
+                var te = new TextEditor(arg.Filepath, sr, () =>
+                {
+                    sw.Flush();
+                    sw.BaseStream.Position = 0;
+                    sw.BaseStream.SetLength(0);
+                    return sw;
+                }, 30);
+                await te.RunAsync();
+            }).Wait();
+            sr.Close();
+            sw.Close();
+        }
+
+        private static void ExecuteWithExternal(EncryptedContainer ec, ModifyArgument arg)
+        {
+            if (OperatingSystem.IsLinux())
+            {
+                var filename = $"/dev/shm/{Path.GetFileName(arg.Filepath)}-{Guid.NewGuid()}";
+                try
+                {
+                    using var fs = File.Create(filename, 8192, FileOptions.DeleteOnClose);
+                    File.SetUnixFileMode(filename, UnixFileMode.UserRead | UnixFileMode.UserWrite);
+                    fs.Write(ec.ContentStream.GetAsReadOnlySpan());
+                    fs.Flush();
+
+                    var ps = Process.Start(arg.Editor!, filename) ?? throw new ApplicationException($"Could not start process {arg.Editor}");
+                    ps.WaitForExit();
+                    if (ps.ExitCode != 0)
+                    {
+                        throw new Exception($"External editor exited with code {ps.ExitCode}, which indicates failure. Abort.");
+                    }
+
+                    fs.Position = 0;
+                    ec.ContentStream.SetLength(0);
+                    fs.CopyTo(ec.ContentStream);
+
+                    fs.Close();
+                    fs.Dispose();
+                }
+                finally
+                {
+                    File.Delete(filename);
+                }
+            }
+            else if (OperatingSystem.IsWindows())
+            {
+                throw new NotImplementedException($"External editors are currently unsupported on Windows.");
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
--- src/EncryptedContainerFile/ECF.CLI/Arguments/ModifyArgument.cs
+++ src/EncryptedContainerFile/ECF.CLI/Arguments/ModifyArgument.cs
@@ -5,5 +5,7 @@ namespace ECF.CLI.Arguments
     [Verb("modify", aliases: new[] { "m" }, HelpText = "Modify the contents of an Encrypted Container File")]
     internal class ModifyArgument : DecryptArgument
     {
+        [Option('e', "editor", Default = null, HelpText = "Use an external editor to edit the file.", Required = false)]
+        public string? Editor { get; protected set; }
     }
 }
